5b4399d2d096f2bbe97d0b381197de2e55cee941
Stefan J. Ossanna <30768274+ossi1993@users.noreply.github.com>
2020-11-25 18:17:40 +0100

    start

---
+++ dist/js/chunk-2d0c4aa2.e3cf4ad8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4aa2"],{"3c95":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},u=[],r=t("2877"),a={},l=Object(r["a"])(a,c,u,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0c4aa2.e3cf4ad8.js.map
